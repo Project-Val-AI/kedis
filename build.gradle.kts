8b16a8d9dfcc5504cee009f9c1754378f5b60ef0
xanderjans <30940652+xanderume@users.noreply.github.com>
2025-06-17 09:50:36 +0200

    add maven publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,24 @@
 plugins {
+    `maven-publish`
     alias(libs.plugins.dokka) apply false
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.kover) apply false
+    id("net.linguica.maven-settings") version "0.5"
+}
+
+subprojects {
+    apply(plugin = "org.gradle.maven-publish")
+    apply(plugin = "net.linguica.maven-settings")
+
+    publishing {
+
+        repositories {
+            maven {
+                name = "fyreGames"
+                url = uri("https://maven.fyre.services/games")
+            }
+        }
+
+
+    }
 }
